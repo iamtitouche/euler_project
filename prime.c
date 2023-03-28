6deef591280332af51d7e10d6515304b4969779c
Titouan SADOUN <titouan.sadoun@grenoble-inp.org>
2023-03-28 12:04:10 +0200

    Ajout de la cfonction concatenate

--- prime.c
+++ prime.c
@@ -12,49 +12,42 @@
 List* prime_factors(int n) {
     List *prime_factors = create_list();
     
+    double sqrt_n = pow((double)n, 0.5);
+
     while (n % 2 == 0) {
         n = n / 2;
         append(prime_factors, 2);
     }
 
-    for (int i = 3; i <= n; i+=2) {
+    for (int i = 3; i <= sqrt_n; i+=2) {
         while (n % i == 0) {
             n = n / i;
             append(prime_factors, i);
         }
+        if (n == 1) break;
     }
+
+    if (n > 1) append(prime_factors, n);
 
     return prime_factors;
 }
 
+
 int main() {
-    int n = 18;
-    List *factors = prime_factors(n);
 
-    ListElement *curr_f = (*factors).head;
-    for (int i = 0; i < (*factors).lenght; i++) {
-        printf("%d\n", (*curr_f).value);
-        curr_f = (*curr_f).next;
-    }
+    int n;
+    List *factors;
+    ListElement *curr_f;
 
-
-     n = 27;
-    factors = prime_factors(n);
-
-    curr_f = (*factors).head;
-    for (int i = 0; i < (*factors).lenght; i++) {
-        printf("%d\n", (*curr_f).value);
-        curr_f = (*curr_f).next;
-    }
-
-
-    n = 145;
-    factors = prime_factors(n);
-
-    curr_f = (*factors).head;
-    for (int i = 0; i < (*factors).lenght; i++) {
-        printf("%d\n", (*curr_f).value);
-        curr_f = (*curr_f).next;
+    for (n = 2; n <= 35; n++) {
+        factors = prime_factors(n);
+        printf("Prime facors of %d are : ", n);
+        curr_f = (*factors).head;
+        for (int i = 0; i < (*factors).lenght; i++) {
+            printf("%d ", (*curr_f).value);
+            curr_f = (*curr_f).next;
+        }
+        printf("\n");
     }
 
     return EXIT_SUCCESS;
