a4e7ea80675f8a4457b6f3415ed1a0e0e4a9e8a1
Titouan SADOUN <titouan.sadoun@grenoble-inp.org>
2023-04-05 13:49:23 +0200

    utilisation du symbole -> dans linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -22,64 +22,64 @@ typedef struct List{
 
 List* create_list() {
     List *list = malloc(sizeof(List));
-    (*list).lenght = 0;
-    (*list).head = NULL;
-    (*list).queue = NULL;
+    list->lenght = 0;
+    list->head = NULL;
+    list->queue = NULL;
     return list;
 }
 
 ListElement* create_list_element(int value) {
     ListElement *element = malloc(sizeof(ListElement));
-    (*element).value = value;
-    (*element).next = NULL;
-    (*element).prev = NULL;
+    element->value = value;
+    element->next = NULL;
+    element->prev = NULL;
     return element;
 }
 
 void append(List *list_pt, int value) {
     ListElement *new_element = create_list_element(value);
-    if ((*list_pt).lenght == 0) {
-        (*list_pt).head = new_element;
+    if (list_pt->lenght == 0) {
+        list_pt->head = new_element;
     }
     else {
-        (*new_element).prev = (*list_pt).queue;
-        (*(*list_pt).queue).next = new_element;
+        new_element->prev = list_pt->queue;
+        (list_pt->queue)->next = new_element;
     }
-    (*list_pt).queue = new_element;
-    (*list_pt).lenght++;
+    list_pt->queue = new_element;
+    list_pt->lenght++;
 }
 
 List* concatenate(List *list_1, List *list_2) {
-    if ((*list_1).lenght == 0) {
+    if (list_1->lenght == 0) {
         return list_2;
     }
-    else if ((*list_2).lenght == 0) {
+    else if (list_2->lenght == 0) {
         return list_1;
     }
     
-    (*list_1).queue = (*list_2).queue;
-    (*(*list_1).queue).next = (*list_2).head;
-    (*(*list_2).head).prev = (*list_1).queue;
+    list_1->queue = list_2->queue;
+    (list_1->queue)->next = list_2->head;
+    (list_2->head)->prev = list_1->queue;
 
     return list_1;
 }
 
 
 void pivot_sort(List *list) {
-    int pivot = (*(*list).head).value;
+    int pivot = (list->head)->value;
     
     List *bellow_pivot = create_list();
     List *above_pivot = create_list();
 
-    ListElement *curr_cell = (*(*list).head).next;
+    ListElement *curr_cell = (list->head)->next;
 
     while (curr_cell != NULL) {
         if ((*curr_cell).value <= pivot) {
-            append(bellow_pivot, (*curr_cell).value);
+            append(bellow_pivot, curr_cell->value);
         }
-        else append(above_pivot, (*curr_cell).value);
+        else append(above_pivot, curr_cell->value);
         
-        curr_cell = (*curr_cell).next;
+        curr_cell = curr_cell->next;
     }
 }
 
